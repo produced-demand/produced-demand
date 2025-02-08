a8ef6977a2374bd13b1ccf852a1b63daa681a5ab
Hamza Nasher-Alneam <hnasheralneam@gmail.com>
2025-02-07 21:38:19 -0500

    Routes and buses appear in correct location, buses loop on circular routes

--- bus-movement.gd
+++ bus-movement.gd
@@ -1,13 +1,29 @@
 extends Node2D
 
 var parent
+var reverse: bool = false
+var route_closed: bool
 
 func _ready() -> void:
 	parent = get_parent()
 	parent.progress_ratio = int(randf())
 
 
+
 func _process(delta: float) -> void:
 	if parent is PathFollow2D:
-		var new_progress = parent.get_progress() + 100 * delta
+		var change = 100 * delta
+
+		if not route_closed:
+			if parent.get_progress_ratio() >= .99:
+				reverse = true
+			elif parent.get_progress_ratio() <= .01:
+				reverse = false
+			if reverse:
+				change *= -1
+			
+		var new_progress = parent.get_progress() + change
 		parent.set_progress(new_progress)
+
+func set_route_closed(value: bool):
+	route_closed = value
--- path-manager.gd
+++ path-manager.gd
@@ -5,6 +5,15 @@ var current_path
 
 var bus_scene = preload("res://bus.tscn")
 
+#===============================
+# paths to do:
+# live preview of placement
+# actually make them show up in the correct place
+#===============================
+
+func _ready() -> void:
+	global_position = Vector2.ZERO
+
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("start_create_path"):
 		creating_path = not creating_path
@@ -13,7 +22,7 @@ func _input(event: InputEvent) -> void:
 			create_path(get_viewport().get_camera_2d().get_global_mouse_position())
 		else:
 			print("...stopping to create path")
-			add_point_to_path(get_viewport().get_camera_2d().get_global_mouse_position())
+			var route_is_closed = add_point_to_path(get_viewport().get_camera_2d().get_global_mouse_position(), true)
 			add_child(current_path)
 
 			var line := Line2D.new()
@@ -27,26 +36,38 @@ func _input(event: InputEvent) -> void:
 			add_child(line)
 
 			# remove after testing is done
-			add_bus_to_route(current_path)
+			add_bus_to_route(current_path, route_is_closed)
 
 
 	elif Input.is_action_just_pressed("left_click") and creating_path:
-		add_point_to_path(get_viewport().get_camera_2d().get_global_mouse_position())
+		add_point_to_path(get_viewport().get_camera_2d().get_global_mouse_position(), false)
 
 
-func create_path(position):
+func create_path(start_position):
 	var line = Path2D.new()
 	current_path = line
 	line.curve = Curve2D.new()
-	line.curve.add_point(position)
+	line.curve.add_point(start_position)
 
-func add_point_to_path(position):
-	current_path.curve.add_point(position)
+func add_point_to_path(point_position, final_point):
+	var last_point = current_path.curve.get_baked_points()[-1]
+	var same_spot = false
+	## check if points are close
+	if abs(last_point.x - point_position.x) <= 50 and abs(last_point.x - point_position.x) <= 50:
+		same_spot = true
+	
+	current_path.curve.add_point(point_position)
+	
+	if final_point and same_spot:
+		return true
+	else:
+		return false
 
-func add_bus_to_route(route):
+func add_bus_to_route(route, route_is_closed):
 	var follow_path = PathFollow2D.new()
 	var bus = bus_scene.instantiate()
 	
+	bus.set_route_closed(route_is_closed)
+	
 	follow_path.add_child(bus)
 	route.add_child(follow_path)
-	print("added bus")
