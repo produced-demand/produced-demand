b0679dcefcb868b2b5118ff5a820a57046f096f5
Hamza Nasher-Alneam <hnasheralneam@gmail.com>
2025-02-12 08:28:49 -0500

    Progress on fixing bus movement

--- scripts/bus-movement.gd
+++ scripts/bus-movement.gd
@@ -8,8 +8,8 @@ var route_closed: bool
 var current_station
 var visited = false
 
-@export var top_speed = 100
-var speed = top_speed
+@export var top_speed = 50
+var speed
 var acceleration
 var max_occupancy = 8
 var occupants: Array
@@ -18,61 +18,119 @@ var time_arrived_at_station = -1
 var wait_time
 
 func _ready() -> void:
+	speed = top_speed
 	parent = get_parent()
 	route = parent.get_parent()
-	parent.progress_ratio = int(randf())
+	parent.progress_ratio = randf()
 
 func _process(delta: float) -> void:
 	var change = speed * delta
 
 	if not route_closed:
-		if parent.get_progress_ratio() >= .99:
+		if parent.get_progress_ratio() >= .98:
 			reverse = true
-		elif parent.get_progress_ratio() <= .01:
+		elif parent.get_progress_ratio() <= .02:
 			reverse = false
 		if reverse:
 			change *= -1
 		
-
+	# if there is no station in range
 	if !current_station:
+		#print("not near stations")
 		for station in route.stations:
-			if global_position.distance_to(station.global_position) < 60:
-				current_station = station
+			if global_position.distance_to(station.global_position) < 80:
 				acceleration = (pow(speed, 2)) / (2 * global_position.distance_to(station.global_position))
 				
 				if station != current_station:
 					current_station = station
+					print("station sighted!")
+					var marker = Sprite2D.new()
+					marker.texture = load("res://assets/station.svg")
+					marker.global_position = global_position	
+					marker.scale = Vector2(.3, .3)
+
+	# if there is a station in range
 	else:
-		speed += acceleration * delta
-		if speed > top_speed + 100:
-			print("error! speed is going in the wrong direction")
-			speed = top_speed
-		
-		if not visited:
-
-			if global_position.distance_to(current_station.global_position) <= 5:
-				if time_arrived_at_station == -1:
-					time_arrived_at_station = Time.get_ticks_msec()
-					wait_time = current_station.get_wait_time(get_open_seats())
-				elif Time.get_ticks_msec() - time_arrived_at_station > wait_time:
-					acceleration = ((pow(top_speed, 2)) / (2 * 80))
-					visited = true
-
-				speed = 0
-				var new_occupants = current_station.get_people(get_open_seats(), route)
-				for occupant in new_occupants:
-					occupants.append(occupant)
-				update_occupants_label()
-
-
-
-		elif visited:			
-			if global_position.distance_to(current_station.global_position) > 60:
+		print(delta)
+		var distance_to_station = global_position.distance_to(current_station.global_position)
+		# station visited, out of range
+		if visited:
+			if distance_to_station > 80:
+				print("station out of range")
 				current_station = null
 				visited = false
 				time_arrived_at_station = -1
 				speed = top_speed
-				acceleration = 0
+		
+		# approaching station (visited == false)
+		else:
+			# arrived
+			if distance_to_station < (parent.get_progress() + change / 2):
+				var marker = Sprite2D.new()
+				marker.texture = load("res://assets/station.svg")
+				marker.global_position = global_position	
+				marker.scale = Vector2(.2, .2)
+				get_tree().get_root().add_child(marker)
+				print("at station")
+				visited = true
+
+
+
+
+				
+				#if time_arrived_at_station == -1:
+					##print("SETTING STATION ARRIVAL TIME")
+					#time_arrived_at_station = Time.get_ticks_msec()
+					#wait_time = current_station.get_wait_time(get_open_seats())
+				#elif Time.get_ticks_msec() - time_arrived_at_station > wait_time:
+					#acceleration = ((pow(top_speed, 2)) / (2 * 80))
+					#visited = true
+					#print("SETTING VISITED TO TRUE")
+
+				#speed = 0
+				#var new_occupants = current_station.get_people(get_open_seats(), route)
+				#for occupant in new_occupants:
+					#occupants.append(occupant)
+				#update_occupants_label()
+
+		
+		
+		
+		#speed += acceleration * delta
+		#if speed > top_speed:
+			##print("error! speed is going in the wrong direction. current speed: " + str(speed) + " top speed: " + str(top_speed))
+			#speed = top_speed
+		
+		#if not visited:
+
+			#if global_position.distance_to(current_station.global_position) <= 10:
+				#visited = true
+				#if time_arrived_at_station == -1:
+					##print("SETTING STATION ARRIVAL TIME")
+					#time_arrived_at_station = Time.get_ticks_msec()
+					#wait_time = current_station.get_wait_time(get_open_seats())
+				#elif Time.get_ticks_msec() - time_arrived_at_station > wait_time:
+					#acceleration = ((pow(top_speed, 2)) / (2 * 80))
+					#visited = true
+					##print("SETTING VISITED TO TRUE")
+#
+				##speed = 0
+				#var new_occupants = current_station.get_people(get_open_seats(), route)
+				#for occupant in new_occupants:
+					#occupants.append(occupant)
+				#update_occupants_label()
+
+
+
+		#elif visited:			
+			#if global_position.distance_to(current_station.global_position) > 80:
+				#print("station out of range")
+				#current_station = null
+				#print(current_station)
+				#visited = false
+				#time_arrived_at_station = -1
+				#speed = top_speed
+				#acceleration = 0
 				
 	var new_progress = parent.get_progress() + change
 	parent.set_progress(new_progress)
